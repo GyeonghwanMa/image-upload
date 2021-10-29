da7fd41690746c394d5ba37492766328770233c2
kyunghwanma <makh1213@naver.com>
2021-10-29 22:27:39 +0900

    챕터3 완료

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const multer = require("multer");
 const { v4: uuid } = require("uuid");
 const mime = require("mime-types");
+const mongoose = require("mongoose");
+const Image = require("./models/Image");
+// const Image = mongoose.model("Image");
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "./uploads"),
   filename: (req, file, cb) =>
@@ -17,15 +22,34 @@ const upload = multer({
     fileSize: 1024 * 1024 * 5,
   },
 });
-
 const app = express();
 const PORT = 5000;
 
-app.use("/uploads", express.static("uploads"));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected.");
 
-app.post("/upload", upload.single("image"), (req, res) => {
-  console.log(req.file);
-  res.json(req.file);
-});
+    app.use("/uploads", express.static("uploads"));
 
-app.listen(PORT, () => console.log("Express server listening on PORT " + PORT));
+    app.post("/images", upload.single("image"), async (req, res) => {
+      const image = await new Image({
+        key: req.file.filename,
+        originalFileName: req.file.originalname,
+      }).save();
+      res.json(image);
+    });
+
+    app.get("/images", async (req, res) => {
+      const images = await Image.find();
+      res.json(images);
+    });
+
+    app.listen(PORT, () =>
+      console.log("Express server listening on PORT " + PORT)
+    );
+  })
+  .catch((err) => console.log(err));
