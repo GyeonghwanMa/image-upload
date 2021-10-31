a9ab24ff6ecec472fdd06fad88500c45ae8584e4
kyunghwanma <makh1213@naver.com>
2021-10-31 22:11:17 +0900

    챕터6 완료

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -34,6 +34,7 @@ userRouter.post("/register", async (req, res) => {
 userRouter.patch("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
+    if (!user) throw new Error("입력하신 정보가 올바르지 않습니다.");
     const isValid = await compare(req.body.password, user.hashedPassword);
     if (!isValid) throw new Error("입력하신 정보가 올바르지 않습니다.");
     user.sessions.push({ createdAt: new Date() });
@@ -59,6 +60,21 @@ userRouter.patch("/logout", async (req, res) => {
     res.json({ message: "user is logged out." });
   } catch (err) {
     console.log(err);
+    res.status(400).json({ message: err.message });
+  }
+});
+
+userRouter.get("/me", (req, res) => {
+  try {
+    if (!req.user) throw new Error("권한이 없습니다.");
+    res.json({
+      message: "success",
+      sessionId: req.headers.sessionid,
+      name: req.user.name,
+      userId: req.user._id,
+    });
+  } catch (err) {
+    console.error(err);
     res.status(400).json({ message: err.message });
   }
 });
